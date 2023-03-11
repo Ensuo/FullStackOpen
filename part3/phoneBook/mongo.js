37c8fd3be8c4650de22aab90e8c8c740c9275525
EnzoH <haradayasuo2010@gmail.com>
2023-03-10 21:04:50 -0300

    Finished 3.12

---
+++ part3/phoneBook/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://haradayasuo2010:${password}@cluster0.sdcwovp.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)  
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if(process.argv.length == 3){
+    Person.
+        find({}).
+        then(persons => 
+            persons.forEach(person => {
+                console.log(person)
+        mongoose.connection.close()
+    }))
+}else if( process.argv.length == 5){
+    const person = new Person({
+        name: name,
+        number: number,
+    })
+
+    person.save().then(result => {
+        console.log(`Added ${name}:${number}`)
+        console.log(result)
+        mongoose.connection.close()
+    })
+}else{
+    console.log("Missing info")
+    process.exit(1)
+}
